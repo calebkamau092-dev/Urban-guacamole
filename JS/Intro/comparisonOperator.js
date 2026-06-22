ae2b7869b3116e6d7c2229f7cd8552f8b178e26f
calebkamau092-dev <calebkamau092@gmail.com>
2026-06-22 10:50:29 -0400

    learnt And,Or,Not Operators

--- JS/Intro/comparisonOperator.js
+++ JS/Intro/comparisonOperator.js
@@ -50,3 +50,24 @@ console.log(
   `val1=${val1}!=val2=${val2}
    Ans ${inequality} its type is ${typeof inequality}`
 );
+
+let numb3 = 20;
+let numb4 = 10;
+
+let str1 = "a";
+let str2 = "b";
+
+console.log(`numb3=${numb3} and numb4=${numb4}`);
+console.log(`str1=${str1} and str2=${str2}`);
+
+console.log(`numb3>numb4 ${numb3 > numb4}`);
+console.log(`str2>str1 ${str1 > str2}`);
+
+console.log(`numb3>=numb4 ${numb3 >= numb4}`);
+console.log(`str2>=str1 ${str1 >= str2}`);
+
+console.log(`numb3<numb4 ${numb4 > numb3}`);
+console.log(`str2<str1 ${str1 < str2}`);
+
+console.log(`numb3<=numb4 ${numb3 <= numb4}`);
+console.log(`str2<=str1 ${str1 <= str2}`);
