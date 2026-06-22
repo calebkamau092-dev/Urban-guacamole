cd0e2484233f4ef3d303984192ac448c80e22a5a
calebkamau092-dev <calebkamau092@gmail.com>
2026-06-21 20:28:51 -0400

    learned priitive datatypes in js

---
+++ JS/Intro/PrimitiveDataType.js
@@ -0,0 +1,36 @@
+let number1=23; //real number
+console.log(number1);
+let number2=34.34; //floating point number
+console.log(number2);
+let number3=-34; //negative number
+console.log(number3);
+let biggestNumber=Number.MAX_VALUE; //largest possible number in JavaScript
+console.log(biggestNumber);
+
+//strings
+let string1="Hello World"; //string with double quotes
+console.log(string1);
+let string2='Hello World'; //string with single quotes
+console.log(string2);
+let string3=`Hello World`; //string with backticks long sentences
+console.log(string3);
+
+//escaping characters
+let string4="Hello \"World\""; //escaping double quotes
+console.log(string4);
+let string5='Hello \'World\''; //escaping single quotes
+console.log(string5);
+let string6=`Hello \`World\``; //escaping backticks
+console.log(string6);
+
+//boolean values
+let johnIsMale=true; //true value
+console.log(johnIsMale);
+let johnIsFemale=false; //false value
+console.log(johnIsFemale);
+
+//null and undefined values lack of value
+let nullValue=null; //null value
+console.log(nullValue);
+let undefinedValue=undefined; //undefined value
+console.log(undefinedValue);       
