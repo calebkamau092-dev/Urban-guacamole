c6c916a98e11faef4109ba70332d1cfac3b089f8
calebkamau092-dev <calebkamau092@gmail.com>
2026-07-05 18:23:12 -0400

    created mycallback function

---
+++ JS/junior-dev/statementsAndExpressions.js
@@ -0,0 +1,10 @@
+//expression-piece of code that produces a value
+let n1 = 23;
+function sayMyName() {
+  console.log(`caleb kamau`);
+}
+console.log(n1);
+console.log(10 + 12);
+console.log(n1 > 10);
+console.log(sayMyName());
+//statements
