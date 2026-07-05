3ca736c5605be5aa0619cff600c555bae42eb669
calebkamau092-dev <calebkamau092@gmail.com>
2026-07-05 18:23:12 -0400

    learnt on recursive functions

---
+++ JS/junior-dev/typesOfFunctions.js
@@ -0,0 +1,23 @@
+function areaCircle(r) {
+  return 3.142 * r * r;
+}
+
+let age = 23;
+let samAge = age;
+let ac = areaCircle;
+ac(10);
+console.log(samAge);
+
+console.log(`value of ac is ${ac} its typeof is ${typeof ac}`);
+//anonymous
+let g = function () {
+  console.log(`hello`);
+};
+
+g();
+
+function currentTime() {
+  const now = new Date();
+  alert("the current time is" + now.toLocaleTimeString());
+}
+currentTime();
