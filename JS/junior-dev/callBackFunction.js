3ae9879f812f0573380cfb2d26203b4cd04a9f71
calebkamau092-dev <calebkamau092@gmail.com>
2026-07-05 18:23:12 -0400

    commits for anonymous and callback functions

---
+++ JS/junior-dev/callBackFunction.js
@@ -0,0 +1,12 @@
+/*function showCurrentTime() {
+  const currentTime = new Date();
+  console.log(`current time is ${currentTime}`);
+}
+
+showCurrentTime();
+setInterval(showCurrentTime, 2000);*/
+
+setInterval(() => {
+  const currentTime = new Date();
+  console.log(`Current time is ${currentTime}`);
+}, 2000);
