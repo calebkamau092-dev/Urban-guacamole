cf52c9a4ed97f63089441c99daa9eead6f122f4a
calebkamau092-dev <calebkamau092@gmail.com>
2026-08-08 12:05:22 -0400

    Completed Github Project

---
+++ REACT/vite-project/src/GithubProject/index.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import TopNav from "./TopNav";
+import InfoSection from "./InfoSection";
+import PersonCard from "./PersonCard";
+
+function GithubProject() {
+  const [people, setPeople] = useState([]);
+
+  // fetch api. axios
+  const getUserData = async () => {
+    try {
+      let response = await axios({
+        method: "GET",
+        url: "https://api.github.com/users",
+        headers: {
+          Authorization: "Bearer YOUR_GITHUB_TOKEN",
+          Accept: "application/vnd.github+json",
+        },
+      });
+
+      setPeople(response.data);
+    } catch (e) {
+      console.log("Error is ", e);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  //Lifecycle. githubProject
+
+  return (
+    <div>
+      <TopNav setPeople={setPeople} />
+
+      <InfoSection people={people} />
+
+      {people.map((person) => (
+        <PersonCard key={person.id} person={person} />
+      ))}
+    </div>
+  );
+}
+
+export default GithubProject;
