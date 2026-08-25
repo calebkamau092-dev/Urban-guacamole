f098e062dc30c7cc434105259c680f51ae86b8d0
calebkamau092-dev <calebkamau092@gmail.com>
2026-08-25 16:16:44 -0400

    did intro to python

---
+++ Python/data_type.py
@@ -0,0 +1,18 @@
+x=10  #integer
+y=3.14 #float
+z="hello" #string
+a=True #boolean
+b=[1,2,3,4] #list <array> mutable <by valu>
+c={1,2,3} #set
+d=(1,2,3) #tuple <list> immutable
+e={"key","value"} #dictionary-->use bracket notation
+
+print("x is","its type is",type(x))
+
+print(f"y is {y} its type is {type(y)}")
+print("z is","its type is",type(z))
+print(f"a is {a} its type is {type(a)}")
+print("b is","its type is",type(b))
+print(f"c is {c} its type is {type(c)}")
+print("d is","its type is",type(d))
+print(f"e is {e} its type is {type(e)}")
---
+++ Python/start.py
@@ -0,0 +1,6 @@
+x=34
+print("X is =", x)
+y=20
+print("Y is =", y)
+z=x+y
+print("Z is =", z)
