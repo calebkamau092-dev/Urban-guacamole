ce7dc0585a70123088850ad1aea795beda771ec4
calebkamau092-dev <calebkamau092@gmail.com>
2026-09-10 23:06:43 -0400

    Static properties

--- PYTHON/object_oriented_programming/bank/account.py
+++ PYTHON/object_oriented_programming/bank/account.py
@@ -1,45 +1,90 @@
-"""
-Abstractions.
-Bank Class→
-deposits, withdrawl,
-show account.
-getter and setter, →
-→ easy to scale function <understing>
-_______________
+# Abstractions.
+# Bank Class->
+# deposits, withdrawal,
+# show account.
+# getter and setter. ->
+# -> easy to scale function <understing>
 
-→ Login account
-→ Create account
-→ Deposit
-→ Withdrawal
-→ account balance
+# -> Login account
+# -> Create account
+# -> Deposit
+# -> withdrawal
+# -> account balance
 
 """
+Static <it does not change>.<class properties> properties, <Belong to the class>
+Static Method.<class method. <Function belongs to the class>
 
+Why would you want to use a class property.
+"""
 
 class BankAccount:
-
+    clients = 0 #static
+    bank_name = "Post Bank" #static property
+    
+    # this -> .<
     def __init__(self, name, balance, account_no):
         self.name = name
         self.balance = balance
         self.account_no = account_no
-
-    # latter change gee
-    def get_balance(self):
-        pass
-
-    # setter
+        #BankAccount.clients = BankAccount.clients + 1
+        #self._class_.clients = self._class_.clients + 1
+        #self._class_.add_client()
+        BankAccount.add_client()
+        #self._class_.bank_name = name_bank
+        #self._class_.clients += 1
+        #BankAccount.clients += self.clients
+        
+    #data i read
+    @property
+    def balance(self):
+        print("somebody tried to read johns balance")
+        return self._balance
+        
+    #to control updated
+    @balance.setter
+    def balance(self, value):
+        if not isinstance(value, (int, float)):
+            print("Ensure you pass a number for new balance")
+            return
+        if value < 0:
+            print("Ensure now balance must not be less than 0")
+            return
+        self._balance = value
+        
+    #Instance method< self->object>
     def deposit(self):
         pass
-
+        
     def withdrawal(self):
         pass
-
+        
     def show_account_details(self):
         print(f"Owner {self.name}")
         print(f"Balance {self.balance}")
         print(f"Account No {self.account_no}")
+        
+    #Static Method.<class method><cls> @staticmethod ->
+    
+    @staticmethod
+    def calculate_interest(amount, year):
+        rate = 10
+        interest_per_year = amount * (rate / 100)
+        interset_total = interest_per_year * year
+        total = amount + interset_total
+        print(f"if you take a loan, of ${amount} , interest rate per year {interest_per_year}")
+        print(f"Total interest {interset_total}, total to pay {total} after {year}")
+        
+    #Class Method. 
+    #class itself.
+    @classmethod
+    def add_client(cls):
+        cls.clients = cls.clients + 1
+        
+john = BankAccount(name="John Mwangi", balance=0, account_no="223344223")
+print("Total clients", BankAccount.clients)
 
+samuel = BankAccount(name="Samuel", balance=0, account_no="223344223")
+print("Total clients", BankAccount.clients)
 
-caleb = BankAccount(name="Caleb kamau", balance=10000, account_no="725528492")
-
-caleb.show_account_details()
+BankAccount.calculate_interest(50000, 3)
--- PYTHON/object_oriented_programming/with_inheritance.py
+++ PYTHON/object_oriented_programming/with_inheritance.py
@@ -1,75 +1,28 @@
-# Shape class.
-# properties and methods common to all shapes.
-# 
-# ----> private, public <----
-# ---> getters and setters <hide the properties <private >>
+class rectangle:
+    def __init__(self,length,width):
+        self.length=length
+        self.width=width
+        self.shape_name="rectangle"
 
-class Shape:
-    def __init__(self, shape_name):
-        self.shape_name = shape_name
-
-    def describe(self):
-        print(f"This is a {self.shape_name}")
-
-    def display_info(self):
-        print("-------------------------------")
-        print(f"Shape: {self.shape_name}")
-        print(f"Area: {self.area()}")
-        print(f"Perimeter: {self.perimeter()}")
-        print("-------------------------------")
-
-    # Method area
+        
     def area(self):
-        print(f"For Shape ${self.shape_name} area calculation missing")
+        area=self.length*self.width
+        print(f"for area of  {self.shape_name} with length{self.length} and width {self.width} area: {area}")
 
-    def perimeter(self):
-        print(f"For Shape ${self.shape_name} perimeter calculation missing")
-
-
-# Inheritance class Name (<class inheriting from>)
-class Rectangle(Shape):
-
-    def __init__(self, length, width):
-        # Name shape shape
-        # super <class we are inheriting from>
-        # self <specific object created the class>
-        super().__init__(shape_name="Rectangle")
-
-        self.length = length
-        self.width = width
-
-    # Method calculating area
+class square:
+    def __init__(self,side):
+        self.length=side
+        self.width=side
+        self.shape_name="square"
     def area(self):
-        return self.length * self.width
-
-    def perimeter(self):
-        return 2 * (self.length + self.width)
+        area=self.length*self.width
+        print(f"for {self.shape_name} of length {self.length} and width {self.width} area: {area}")
 
 
-class Triangle(Shape):
+r1=rectangle(length=20,width=10)
+r1.area()
+print(r1.shape_name)
 
-    def __init__(self, base, height):
-        super().__init__(shape_name="Triangle")
-
-
-# code for the square class
-class Square(Rectangle):
-    def __init__(self, side):
-        # super -> its not shape its Rectangle
-        super().__init__(length=side, width=side)
-        self.shape_name = "Square"
-
-
-s1 = Square(side=35)
-print("Shape name", s1.shape_name)  # shape name
-print("Area is", s1.area())  # Square -> Rectangle -> Shape
-s1.describe()  # Shape
-s1.display_info()  # shape
-# r1 = Rectangle(length=20, width=12)
-
-# print("Shape name", r1.shape_name) # shape name
-# print("Area is", r1.area()) # Rectangle.area <>
-# r1.describe() # Shape
-# r1.display_info() # shape
-t1 = Triangle(20, 30)
-t1.area()  # Triangle -> Shape
+s1=square(side=12)
+s1.area()
+print(s1.shape_name)
